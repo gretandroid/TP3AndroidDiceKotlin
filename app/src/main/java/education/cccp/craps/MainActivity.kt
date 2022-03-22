00b0d5d0f1c3613f42d1944f446cd32e897810d0
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-22 16:37:34 +0100

    commit draft

--- app/src/main/java/education/cccp/craps/MainActivity.kt
+++ app/src/main/java/education/cccp/craps/MainActivity.kt
@@ -2,10 +2,29 @@ package education.cccp.craps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.VISIBLE
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun showDices() {
+        findViewById<ImageView>(imageViewFirstDice).run {
+            if (!isVisible) visibility = VISIBLE
+        }
+        findViewById<ImageView>(imageViewSecondDice).run {
+            if (!isVisible) visibility = VISIBLE
+        }
+    }
+
+    fun onClickRollDiceButton(view: View) {
+        showDices()
+    }
+
+    fun rollDice(): String = "1"
 }
